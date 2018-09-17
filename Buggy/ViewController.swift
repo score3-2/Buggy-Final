12bbb258650d97fb8b0df91e9e692bf5d90f3d19
score3-2 <34635290+score3-2@users.noreply.github.com>
2018-09-17 13:40:17 -0700

    Final Chapter 9

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Button
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Method: \(#function), In file: \(#file), line: \(#line) ")
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        // Go one step too far emptying the array (notice the range change):
+        for _ in 0..<10 {
+            array.removeObject(at: 0)
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
